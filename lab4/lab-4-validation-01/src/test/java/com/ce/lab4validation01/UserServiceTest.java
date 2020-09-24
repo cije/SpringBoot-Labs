5f8ae441f18fa42f8371163164c7809f2bcb4ccf
cije <an37500@163.com>
2020-09-24 19:15:12 +0800

    手动校验

--- lab4/lab-4-validation-01/src/test/java/com/ce/lab4validation01/UserServiceTest.java
+++ lab4/lab-4-validation-01/src/test/java/com/ce/lab4validation01/UserServiceTest.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
 /**
  * @author c__e
  * @date Created in 2020/9/24 9:19
@@ -46,4 +50,24 @@ public class UserServiceTest {
         UserAddDTO addDTO = new UserAddDTO();
         service.add02(addDTO);
     }
+
+    @Autowired
+    private Validator validator;
+
+    @Test
+    public void testValidator() {
+        // 打印，查看validator的类型
+        System.out.println(validator);
+
+        // 创建UserAddDTO 对象
+        UserAddDTO addDTO = new UserAddDTO();
+        // 校验
+        Set<ConstraintViolation<UserAddDTO>> result = validator.validate(addDTO);
+        // 打印校验结果
+        for (ConstraintViolation<UserAddDTO> constraintViolation : result) {
+            // 属性：消息
+            System.out.println(constraintViolation.getPropertyPath() + ":" + constraintViolation.getMessage());
+        }
+    }
+
 }
